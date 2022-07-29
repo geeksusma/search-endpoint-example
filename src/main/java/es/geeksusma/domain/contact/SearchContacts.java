7e20245d93c9bb17b1cd4f8f7c9a6dd0f195eef2
geeksusma-cdr <jesus.villar@codurance.com>
2022-07-29 13:07:58 +0200

    adding the count since it is needed

--- src/main/java/es/geeksusma/domain/contact/SearchContacts.java
+++ src/main/java/es/geeksusma/domain/contact/SearchContacts.java
@@ -2,5 +2,7 @@ package es.geeksusma.domain.contact;
 
 public interface SearchContacts {
 
+    int count(String name, String phone);
+
     Contacts search(String name, String phone, Page page);
 }
--- src/main/java/es/geeksusma/domain/contact/SearchContactsComponent.java
+++ src/main/java/es/geeksusma/domain/contact/SearchContactsComponent.java
@@ -11,6 +11,11 @@ public class SearchContactsComponent implements SearchContacts {
     }
 
     @Override
+    public int count(String name, String phone) {
+        return contactsRepository.countHits(name, phone);
+    }
+
+    @Override
     public Contacts search(String name, String phone, Page page) {
         return contactsRepository.search(name, phone, page);
     }
