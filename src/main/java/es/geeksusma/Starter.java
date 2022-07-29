cb89b4d61c4803440ca0e1ed83d49ab5fb63df0b
geeksusma-cdr <jesus.villar@codurance.com>
2022-07-29 13:34:46 +0200

    adding a starter for the app

---
+++ src/main/java/es/geeksusma/Starter.java
@@ -0,0 +1,13 @@
+package es.geeksusma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class Starter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Starter.class, args);
+    }
+}
